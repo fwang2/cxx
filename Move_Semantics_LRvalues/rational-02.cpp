b0f1e199c4502f1c267fbdb6f78dd272f4b7014f
Feiyi Wang <fwang2@gmail.com>
2021-09-02 09:46:46 -0400

    style update

--- Move_Semantics_LRvalues/rational-02.cpp
+++ Move_Semantics_LRvalues/rational-02.cpp
@@ -4,130 +4,165 @@
 #include <string>
 #include <utility>
 
-void message(const char * s) {
+void message(const char *s)
+{
     puts(s);
     fflush(stdout);
 }
 
 // this version has messages to show which constructor (ctor) is being called.
-class Rational {
+class Rational
+{
     int _n = 0;
     int _d = 1;
-    
+
     // c_str buffer
     static const int _bufsize = 128;
-    mutable char * _buf = nullptr;
-    
-public:
-    Rational() { reset(); message("default ctor"); }
-    Rational(const int & numerator) : _n(numerator), _d(1) { message("int ctor"); }
-    Rational(const int & numerator, const int & denominator) : _n(numerator), _d(denominator) { message("int,int ctor"); }
-    Rational(const Rational & other) : _n(other._n), _d(other._d) { message("copy ctor"); }
+    mutable char *_buf = nullptr;
 
+  public:
+    Rational()
+    {
+        reset();
+        message("default ctor");
+    }
+    Rational(const int &numerator) : _n(numerator), _d(1)
+    {
+        message("int ctor");
+    }
+    Rational(const int &numerator, const int &denominator) : _n(numerator), _d(denominator)
+    {
+        message("int,int ctor");
+    }
+    Rational(const Rational &other) : _n(other._n), _d(other._d)
+    {
+        message("copy ctor");
+    }
 
     // NOTE: to support move semantic, we must define move ctor
     // this is the signature, noexcept is must
 
     Rational(Rational &&) noexcept;
-    
-    
+
     ~Rational();
     void reset();
-    inline int numerator() const { return _n; }
-    inline int denominator() const { return _d; }
-    Rational & operator = (const Rational &);
-    Rational operator + (const Rational &) const;
-    Rational operator - (const Rational &) const;
-    Rational operator * (const Rational &) const;
-    Rational operator / (const Rational &) const;
-    operator std::string () const;
+    inline int numerator() const
+    {
+        return _n;
+    }
+    inline int denominator() const
+    {
+        return _d;
+    }
+    Rational &operator=(const Rational &);
+    Rational operator+(const Rational &) const;
+    Rational operator-(const Rational &) const;
+    Rational operator*(const Rational &) const;
+    Rational operator/(const Rational &) const;
+    operator std::string() const;
     std::string string() const;
-    const char * c_str() const;
+    const char *c_str() const;
 };
-
 
 // NOTE: this is the definition of move ctor
 // we use std::move() to move individual member
 // finally, we reset the rhs.
 
-Rational::Rational(Rational && rhs) noexcept {
+Rational::Rational(Rational &&rhs) noexcept
+{
     message("move ctor");
     _n = std::move(rhs._n);
     _d = std::move(rhs._d);
-    rhs.reset();  // set reset definition; the goal is to leave source object in a sane state
+    rhs.reset(); // set reset definition; the goal is to leave source object in
+                 // a sane state
 }
 
-Rational::~Rational() {
+Rational::~Rational()
+{
     message("dtor");
     reset();
 }
 
-void Rational::reset() {
-    _n = 0; _d = 1;
-    if(_buf) delete [] _buf;
+void Rational::reset()
+{
+    _n = 0;
+    _d = 1;
+    if (_buf)
+        delete[] _buf;
     _buf = nullptr;
 }
 
-Rational & Rational::operator = ( const Rational & rhs ) {
-    if( this != &rhs ) {
+Rational &Rational::operator=(const Rational &rhs)
+{
+    if (this != &rhs)
+    {
         _n = rhs.numerator();
         _d = rhs.denominator();
     }
     return *this;
 }
 
-Rational Rational::operator + ( const Rational & rhs ) const {
+Rational Rational::operator+(const Rational &rhs) const
+{
     return Rational((_n * rhs._d) + (_d * rhs._n), _d * rhs._d);
 }
 
-Rational Rational::operator - ( const Rational & rhs ) const {
+Rational Rational::operator-(const Rational &rhs) const
+{
     return Rational((_n * rhs._d) - (_d * rhs._n), _d * rhs._d);
 }
 
-Rational Rational::operator * ( const Rational & rhs ) const {
+Rational Rational::operator*(const Rational &rhs) const
+{
     return Rational(_n * rhs._n, _d * rhs._d);
 }
 
-Rational Rational::operator / ( const Rational & rhs ) const {
+Rational Rational::operator/(const Rational &rhs) const
+{
     return Rational(_n * rhs._d, _d * rhs._n);
 }
 
-Rational::operator std::string () const {
+Rational::operator std::string() const
+{
     return string();
 }
 
-std::string Rational::string () const {
+std::string Rational::string() const
+{
     return std::string(c_str());
 }
 
-const char * Rational::c_str() const {
-    if(!_buf) _buf = new char[_bufsize]();
+const char *Rational::c_str() const
+{
+    if (!_buf)
+        _buf = new char[_bufsize]();
     snprintf(_buf, _bufsize, "%d/%d", _n, _d);
     return _buf;
 }
 
-int main() {
-    Rational a = 7;     // 7/1
-    Rational b(5, 3);   // 5/3
-    Rational c = b;     // copy ctor
+int main()
+{
+    Rational a = 7;   // 7/1
+    Rational b(5, 3); // 5/3
+    Rational c = b;   // copy ctor
 
     // NOTE: without move ctor, copy ctor will be called
     // Now that we have move ctor, we will observe move ctor is called.
     //
     // std::move() cast the object c as a Rvalue
     // you can change
-    // std::move(c) 
+    // std::move(c)
     // as
     // (Rational &&)(c)
-    // 
+    //
     Rational d = std::move(c);
 
     printf("a is: %s\n", a.c_str());
     printf("b is: %s\n", b.c_str());
     printf("c is: %s\n", c.c_str());
     printf("d is: %s\n", d.c_str());
-    
+
     printf("%s + %s is %s\n", a.c_str(), b.c_str(), Rational(a + b).c_str());
-    
+
     return 0;
 }
